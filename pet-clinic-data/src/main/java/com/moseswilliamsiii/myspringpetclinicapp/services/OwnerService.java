c418821da817a28282ad4cbc4039ecc4736c2766
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-02 13:28:47 -0400

    Refactor service interface to common interface. closes #16.

--- pet-clinic-data/src/main/java/com/moseswilliamsiii/myspringpetclinicapp/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/moseswilliamsiii/myspringpetclinicapp/services/OwnerService.java
@@ -4,13 +4,6 @@ import com.moseswilliamsiii.myspringpetclinicapp.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner finbById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
